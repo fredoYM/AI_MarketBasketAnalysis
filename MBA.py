8e93baeeb45d998268d8a0197358cb15487781ac
Alfred Sunny <112569782+fredoYM@users.noreply.github.com>
2023-10-18 20:54:45 +0100

    Add association rules and apriori algorithm
    Added the association rules for the dataset after preprocessing.
    Implemented the apriori algorithm to find out the frequent itemsets

--- MBA.py
+++ MBA.py
@@ -9,6 +9,18 @@ df.describe(include='all')
 
 df['Total_Price'] = df.Quantity * df.Price
 
+# Remove rows where the 'Itemname' column contains "Adjust bad debt"
+df = df[df['Itemname'] != "Adjust bad debt"]
+
+# Filter the DataFrame to exclude rows where 'Itemname' is missing (not NaN)
+df = df[df['Itemname'].notna()]
+
+# Remove rows where 'Quantity' is less than 1
+df = df[df['Quantity'] >= 1]
+
+# Remove rows where the price is zero
+df = df[df['Price'] != 0]
+
 # Plotting the top 10 most sold products by quantity
 df.groupby('Itemname')['Quantity'].sum().sort_values(ascending=False)[:10].plot(kind='barh', title='Number of Quantity Sold')
 plt.ylabel('Item Name')
@@ -20,3 +32,26 @@ df['Itemname'].value_counts(ascending=False)[:10].plot(kind='barh', title='Numbe
 plt.ylabel('Item Name')
 plt.xlim(1000, 2300)
 plt.show()
+
+# Assign the original DataFrame to df2
+df2 = df
+
+# Create a pivot table using the filtered DataFrame
+pivot_table = pd.pivot_table(df2[['BillNo','Itemname']], index='BillNo', columns='Itemname', aggfunc=lambda x: True, fill_value=False)
+
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
+# Generate frequent itemsets with minimum support of 0.1 (10%)
+frequent_itemsets = apriori(pivot_table, min_support=0.01,use_colnames=True)
+
+# Generate association rules
+rules = association_rules(frequent_itemsets, "confidence", min_threshold = 0.5)
+
+# Print frequent itemsets
+print("Frequent Itemsets:")
+print(frequent_itemsets)
+
+# Print association rules
+print("\nAssociation Rules:")
+rules
